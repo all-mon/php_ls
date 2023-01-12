56b16517f3075fab285c92a7be82d79cd0328fb7
m0n7h0ff <al.monah.off@gmail.com>
2023-01-12 23:13:49 +0500

    Add: route, controller, model and migration

--- laravel_ls_repeat/app/Http/Controllers/DeviceController.php
+++ laravel_ls_repeat/app/Http/Controllers/DeviceController.php
@@ -2,13 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Device;
 use Illuminate\Http\Request;
 
 class DeviceController extends Controller
 {
     public function getById($id)
     {
-        dd($id);
+        $device = Device::find($id);
+        dump($device->name);
+        dump($device->system);
+        dd($device->description);
     }
     public function getAll()
     {
